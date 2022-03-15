895b7caf9ab4fed261b8919cd9d02fa26a9a714c
Marcelo Puebla <marcenp23@gmail.com>
2022-03-15 17:36:36 -0300

    Creado componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./modules/login/login.module').then((m) => m.LoginModule)
+      }
+    ]
+  },
+  { path: '**', component: LoginComponent } //TODO: Crear page not found component
+];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
